d72abcc0059543bd7c1ba23d96acbb4a57f7b02f
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2020-08-08 12:45:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
 RotaryEncoder.onRotateEvent(RotationDirection.Left, function () {
-	
+    spinPercent += -1
+    led.plotBarGraph(
+    spinPercent,
+    100
+    )
 })
 RotaryEncoder.onRotateEvent(RotationDirection.Right, function () {
-	
+    spinPercent += 1
+    led.plotBarGraph(
+    spinPercent,
+    100
+    )
 })
+let spinPercent = 0
 RotaryEncoder.init(Pins.P14, Pins.P14, Pins.P13)
-servos.P0.setStopOnNeutral(true)
+servos.P0.stop()
+spinPercent = 0
+serial.writeNumber(spinPercent)
+led.plotBarGraph(
+50,
+100
+)
 basic.forever(function () {
 	
 })
