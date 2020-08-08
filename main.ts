37cc4d2272d50e3a0fc4717aacfcd98be800d3cc
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2020-08-08 12:38:06 +0100

    initial

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+RotaryEncoder.onRotateEvent(RotationDirection.Left, function () {
+	
+})
+RotaryEncoder.onRotateEvent(RotationDirection.Right, function () {
+	
+})
+RotaryEncoder.init(Pins.P14, Pins.P14, Pins.P13)
+servos.P0.setStopOnNeutral(true)
+basic.forever(function () {
+	
+})
